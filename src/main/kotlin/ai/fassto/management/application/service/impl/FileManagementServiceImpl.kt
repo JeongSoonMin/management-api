cdd5040a54a9198aab8249c6a166612633e5e80f
정순민 <>
2024-05-13 16:37:47 +0900

    불필요 import 제거.

--- src/main/kotlin/ai/fassto/management/application/service/impl/FileManagementServiceImpl.kt
+++ src/main/kotlin/ai/fassto/management/application/service/impl/FileManagementServiceImpl.kt
@@ -10,8 +10,6 @@ import com.amazonaws.HttpMethod
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
 import lombok.extern.slf4j.Slf4j
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
